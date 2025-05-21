a6b85d92fe99c6c6338b8bf4107b7e778007feac
artsemyn <13.kalisna@smpialazhar21.sch.id>
2025-05-21 09:19:52 +0700

    Account Fragment

--- app/src/main/java/com/example/nontondulu/AccountFragment.java
+++ app/src/main/java/com/example/nontondulu/AccountFragment.java
@@ -2,6 +2,8 @@ package com.example.nontondulu;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -11,10 +13,16 @@ import android.view.ViewGroup;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class AccountFragment extends Fragment {
 
@@ -25,11 +33,11 @@ public class AccountFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         // Inisialisasi
-        TextView tvName = view.findViewById(R.id.tv_name);
-        TextView tvEmail = view.findViewById(R.id.tv_email);
-        Button btnLogout = view.findViewById(R.id.btn_logout);
-        TabLayout tabLayout = view.findViewById(R.id.tab_layout);
-        ViewPager2 viewPager = view.findViewById(R.id.view_pager);
+        TextView tvName = view.findViewById(R.id.userName);
+        TextView tvEmail = view.findViewById(R.id.tvEmail);
+        Button btnLogout = view.findViewById(R.id.btnLogout);
+        TabLayout tabLayout = view.findViewById(R.id.tabLayout);
+        ViewPager2 viewPager = view.findViewById(R.id.viewPager);
 
         // Set data dummy
         tvName.setText("Mas Musa");
@@ -44,9 +52,11 @@ public class AccountFragment extends Fragment {
         ).attach();
 
         // Logout
-        btnLogout.setOnClickListener(v -> {
-            // Aksi logout nanti
-        });
+        btnLogout.setOnClickListener(v -> logout());
+    }
+
+    private void logout() {
+        ((MainActivity) requireActivity()).logout();
     }
 }
 
