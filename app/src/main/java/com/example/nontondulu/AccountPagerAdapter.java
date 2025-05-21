0cff45f4203fcc397dcfaf1317a12a28315dbb6a
artsemyn <13.kalisna@smpialazhar21.sch.id>
2025-05-21 10:02:31 +0700

    layout

--- app/src/main/java/com/example/nontondulu/AccountPagerAdapter.java
+++ app/src/main/java/com/example/nontondulu/AccountPagerAdapter.java
@@ -1,11 +1,6 @@
 package com.example.nontondulu;
-import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 public class AccountPagerAdapter extends FragmentStateAdapter {
 
@@ -16,7 +11,7 @@ public class AccountPagerAdapter extends FragmentStateAdapter {
     @NonNull
     @Override
     public Fragment createFragment(int position) {
-        return position == 0 ? new WishlistFragment() : new ResepkuFragment();
+        return position == 0 ? new SavedFragment() : new ResepkuFragment();
     }
 
     @Override
